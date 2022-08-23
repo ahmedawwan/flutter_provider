f2ddd228b89a486afa24a34841163942a632c9c2
Ahmed Saeed <ahmed.saeed@ropstam.com>
2022-08-23 11:19:19 +0500

    UI code modulated

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,59 +10,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     var counterProvider = context.watch<CounterProvider>();
+
+    final List<Map<String, dynamic>> listCounterCards = [
+      {
+        'decrement': counterProvider.decrementCounter1,
+        'increment': counterProvider.incrementCounter1,
+        'value': counterProvider.counter1,
+      },
+      {
+        'decrement': counterProvider.decrementCounter2,
+        'increment': counterProvider.incrementCounter2,
+        'value': counterProvider.counter2,
+      },
+      {
+        'decrement': counterProvider.decrementCounter3,
+        'increment': counterProvider.incrementCounter3,
+        'value': counterProvider.counter3,
+      }
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Provider Example'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          counterCard(
-            counterProvider.decrementCounter1,
-            counterProvider.incrementCounter1,
-            counterProvider.counter1,
-          ),
-          counterCard(
-            counterProvider.decrementCounter2,
-            counterProvider.incrementCounter2,
-            counterProvider.counter2,
-          ),
-          counterCard(
-            counterProvider.decrementCounter3,
-            counterProvider.incrementCounter3,
-            counterProvider.counter3,
-          ),
+          ...listCounterCards.map((element) => counterCard(element)).toList(),
           const SizedBox(height: 50),
-          Text('Sum: ${counterProvider.sum}'),
+          Expanded(
+            child: Text(
+              'Sum: ${counterProvider.sum}',
+              style: const TextStyle(
+                fontSize: 32,
+              ),
+          ),
+          ),
         ],
       ),
     );
   }
 
-  Card counterCard(Function decrement, Function increment, int counter) {
-    return Card(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          //decrement counter
-          IconButton(
-            icon: const Icon(Icons.minimize),
-            onPressed: () {
-              decrement();
-            },
-          ),
-          //counter value
-          Text('$counter'),
-          // increment counter
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              increment();
-            },
-          ),
-        ],
+  Widget counterCard(Map<String, dynamic> counterItem) {
+    return Expanded(
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //decrement counter
+            IconButton(
+              icon: const Icon(Icons.minimize),
+              onPressed: counterItem['decrement'],
+            ),
+            //counter value
+            Text(counterItem['value'].toString()),
+            // increment counter
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: counterItem['increment'],
+            ),
+          ],
+        ),
       ),
     );
   }
